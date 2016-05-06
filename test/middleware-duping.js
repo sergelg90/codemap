bcc863bb2a6335ffc157521cdb32f0cc82c4c464
Carlos Rodriguez <carlos@s8f.org>
2016-05-06 14:24:49 -0700

    simplifying

--- test/middleware-duping.js
+++ test/middleware-duping.js
@@ -29,57 +29,49 @@ describe('middleware duping', function () {
         'middleware[10]': [],
         'middleware[0]': [],
         'middleware[]': []
+      },
+      {
+        _ns: 'test',
+        'middleware[]': []
+      },
+      {
+        _ns: 'test',
+        'middleware[]': []
+      },
+      {
+        _ns: 'test',
+        'middleware[-60]': ['buffet']
+      },
+      {
+        _ns: 'test',
+        'middleware.templ': function container (get, set) {
+          set('bar', 'baz')
+          return 'templ'
+        },
+        'middleware[-60]': ['#middleware.templ', 'foo']
       }
     ]
 
-    function motley () {
-      var rootMap = {
-        _maps: core.concat([].slice.call(arguments)),
-        'listen': function container (get, set) {
-          return function runListen (cb) {
-            require('run-series')(get('test:hooks.listen'), function (err) {
-              if (err) return cb(err)
-              set('foo', 'bar')
-              cb()
-            })
-          }
-        },
-
-        'get': function container (get, set) {
-          return get
-        },
-        'set': function container (get, set) {
-          return set
+    var rootMap = {
+      _maps: core,
+      'listen': function container (get, set) {
+        return function runListen (cb) {
+          require('run-series')(get('test:hooks.listen'), function (err) {
+            if (err) return cb(err)
+            set('foo', 'bar')
+            cb()
+          })
         }
+      },
+
+      'get': function container (get, set) {
+        return get
+      },
+      'set': function container (get, set) {
+        return set
       }
-      return codemap(rootMap).export()
     }
-
-    var app = motley({
-      _ns: 'test',
-      _maps: [
-        {
-          _ns: 'test',
-          'middleware[]': []
-        },
-        {
-          _ns: 'test',
-          'middleware[]': []
-        },
-        {
-          _ns: 'test',
-          'middleware[-60]': ['buffet']
-        },
-        {
-          _ns: 'test',
-          'middleware.templ': function container (get, set) {
-            set('bar', 'baz')
-            return 'templ'
-          },
-          'middleware[-60]': ['#middleware.templ', 'foo']
-        }
-      ]
-    })
+    var app = codemap(rootMap).export()
 
     app.listen(function (err) {
       if (err) return done(err)
